414a05a78eccd20fe5bdabe793704b40f995ad03
zky <1239647229@qq.com>
2018-05-02 17:46:11 +0800

    echoServer完成

--- src/main/java/com/Test/demo/test/server/EchoServer.java
+++ src/main/java/com/Test/demo/test/server/EchoServer.java
@@ -1,5 +1,15 @@
 package com.Test.demo.test.server;
 
+import java.net.InetSocketAddress;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class EchoServer {
 	private final int port;
 
@@ -13,5 +23,26 @@ public class EchoServer {
 			return;
 		}
 		int port = Integer.parseInt(args[0]);
+		new EchoServer(port).start();
+	}
+	
+	public void start() throws Exception{
+		final EchoServerHandler serverHandler =new EchoServerHandler();
+		EventLoopGroup group =new NioEventLoopGroup();
+		try {
+			ServerBootstrap b=new ServerBootstrap();
+			b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port)).childHandler(new ChannelInitializer<SocketChannel>(){
+
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(serverHandler);
+				}
+				
+			});
+			ChannelFuture f=b.bind().sync();
+			f.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully().sync();
+		}
 	}
 }
